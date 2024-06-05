5c4bbae6ee05567e3272371e6483022225e63249
Feer-cabral <cabralfernando782@gmail.com>
2024-06-05 11:30:11 -0300

    Realizo ajuste en la configuracion de la variable de entorno

--- src/utils/commander.js
+++ src/utils/commander.js
@@ -1,8 +1,6 @@
 const { Command } = require("commander");
 const program = new Command();
 
-//1- comando 2- descripcion 3- valor por defecto
-
 program.option("--mode <mode>", "modo de trabajo", "desarrollo");
 program.parse();
 
